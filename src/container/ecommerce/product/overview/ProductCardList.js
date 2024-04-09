0363099ec57176740a2a85f87e70df3d78ecfa3d
Dipak Patil <dipakup.a04@gmail.com>
2024-04-09 09:58:28 +0530

    Product Grid View Completed

--- src/container/ecommerce/product/overview/ProductCardList.js
+++ src/container/ecommerce/product/overview/ProductCardList.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Rate, Row, Col } from 'antd';
+import { Row, Col } from 'antd';
 import UilShoppingBag from '@iconscout/react-unicons/icons/uil-shopping-bag';
-import UilHeart from '@iconscout/react-unicons/icons/uil-heart';
+// import UilHeart from '@iconscout/react-unicons/icons/uil-heart';
 // import { useDispatch } from 'react-redux';
-import { Link, NavLink } from 'react-router-dom';
+// import { Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { ReactSVG } from 'react-svg';
+// import { ReactSVG } from 'react-svg';
 import Heading from '../../../../components/heading/heading';
 import { Button } from '../../../../components/buttons/buttons';
 import { ProductCard } from '../../Style';
 // import { updateWishList } from '../../../../redux/product/actionCreator';
 
 const ProductCardsList = React.memo(({ product }) => {
-  const { id, name, rate, price, oldPrice, popular, img, description } = product;
-  //   const dispatch = useDispatch();
+  const filepathpreffix = 'http://103.67.238.230:1386/';
+  const { Item_Id: id, Item_Name: name, SalePrice1: price, Gallary: gallery } = product;
+
+  /* eslint-disable-next-line no-unsafe-optional-chaining */
+  const productImage = filepathpreffix + gallery[0]?.Filepath;
 
   return (
     <ProductCard className="list-view" style={{ marginBottom: 20 }}>
@@ -21,20 +24,21 @@ const ProductCardsList = React.memo(({ product }) => {
         <Row gutter={15}>
           <Col md={6} xs={24}>
             <figure>
-              <img style={{ width: '100%' }} src={require(`../../../../${img}`)} alt="" />
+              <img src={productImage} alt={name} />
             </figure>
           </Col>
           <Col md={12} xs={24}>
             <div className="product-single-description">
               <Heading className="product-single-title" as="h5">
-                <NavLink to={`/admin/ecommerce/productDetails/${id}`}>{name}</NavLink>
+                {/* <NavLink to={`/admin/ecommerce/productDetails/${id}`}>{name}</NavLink> */}
+                {name} {id}
               </Heading>
-              <p>{description}</p>
+              <p>description</p>
             </div>
           </Col>
           <Col md={6} xs={24}>
             <div className="product-single-info">
-              <Link
+              {/* <Link
                 // onClick={() => dispatch(updateWishList(id))}
                 className={popular ? 'btn-heart favourite' : 'btn-heart'}
                 to="#"
@@ -44,18 +48,17 @@ const ProductCardsList = React.memo(({ product }) => {
                 ) : (
                   <UilHeart />
                 )}
-              </Link>
+              </Link> */}
               <p className="product-single-price">
-                <span className="product-single-price__new">${price} </span>
-                {oldPrice && (
+                <span className="product-single-price__new">₹{price} </span>
+                {price && (
                   <>
-                    <del> ${oldPrice} </del>
+                    <del> ${price} </del>
                     <span className="product-single-price__offer"> 60% Off</span>
                   </>
                 )}
               </p>
               <div className="product-single-rating">
-                <Rate allowHalf defaultValue={rate} disabled /> 4.9
                 <span className="total-reviews"> 778 Reviews</span>
               </div>
               <div className="product-single-action">
